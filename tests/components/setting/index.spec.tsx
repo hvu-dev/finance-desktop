62ee8fabbf028b81135937e10e8cf593cd68bea7
Huy Vu <vuquanghuy2k@gmail.com>
2025-05-17 13:35:03 +0700

    Rearrange import and update gitignore to exclude idea folder

--- tests/components/setting/index.spec.tsx
+++ tests/components/setting/index.spec.tsx
@@ -1,6 +1,7 @@
-import SettingComponent from '@components/setting';
 import { describe, expect, it } from 'vitest';
 import { render } from 'vitest-browser-react';
+
+import SettingComponent from '@components/setting';
 
 describe('SettingComponent', () => {
     it('should render properly', () => {
